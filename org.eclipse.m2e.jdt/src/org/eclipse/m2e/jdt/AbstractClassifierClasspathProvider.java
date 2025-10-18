004e168c1fa57e3248581d3f8a10396a32e13521
Hannes Wellmann <wellmann.hannes1@gmx.net>
2025-10-18 09:47:16 +0200

    A dummy change

--- org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/AbstractClassifierClasspathProvider.java
+++ org.eclipse.m2e.jdt/src/org/eclipse/m2e/jdt/AbstractClassifierClasspathProvider.java
@@ -57,6 +57,7 @@ public abstract class AbstractClassifierClasspathProvider
   protected void addTestFolder(Set<IRuntimeClasspathEntry> runtimeClasspath, IMavenProjectFacade mavenProjectFacade,
       IProgressMonitor monitor, int classpathProperty) throws CoreException {
     Build build = mavenProjectFacade.getMavenProject(monitor).getBuild();
+
     final Set<IPath> allTestClasses = new LinkedHashSet<>();
     allTestClasses.add(mavenProjectFacade.getProjectRelativePath(build.getTestOutputDirectory()));
     addFolders(runtimeClasspath, mavenProjectFacade.getProject(), allTestClasses, classpathProperty);
